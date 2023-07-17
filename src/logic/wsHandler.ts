4709eff3f4c0fda304eef490d16d97cee1f8e3ab
fnordhusen <felix.nordhusen@senacor.com>
2023-07-17 09:45:55 +0200

    Improve spacing in WS responses

--- src/logic/wsHandler.ts
+++ src/logic/wsHandler.ts
@@ -3,29 +3,34 @@ import { storeSubscription, unsubscribeClient } from '../store/subscriptions';
 import { removeClient, writeClient } from '../store/wsClients';
 import { getFromStorage } from '../store/storage';
 
+const objectToJsonString = (object: JsonNode): string => JSON.stringify(object, undefined, 1)
+  .replace(/\n/g, ' ') // Replace all line breaks. We want a single line return item
+  .replace(/\s+/g, ' ') // Reduce all whitespaces to one character.
+
 export const sendWsMessage = (webSocket: WebSocketWithId, type: WsOutboundMessageType, message?: JsonNode) => {
-    const wrappedMessage: WsOutboundMessage = {type, body: message, _clientId: webSocket.clientId}
-    webSocket.socket.send(JSON.stringify(wrappedMessage))
-}
+  const wrappedMessage: WsOutboundMessage = {type, body: message, _clientId: webSocket.clientId};
+  webSocket.socket.send(objectToJsonString(wrappedMessage));
+};
 
 export class WsHandler {
   private readonly webSocket: WebSocketWithId;
+
   constructor(webSocket: WebSocketWithId) {
-    this.webSocket = webSocket
+    this.webSocket = webSocket;
   }
 
   public get clientId() {
-    return this.webSocket.clientId
+    return this.webSocket.clientId;
   }
 
   public handleMessage = (message: string) => {
     try {
-        this.handleMessageInternal(message)
-      } catch (e) {
-        console.error(e);
-        this.sendMessage('ERROR', `${(e as Error).message}`);
-      }
-  }
+      this.handleMessageInternal(message);
+    } catch (e) {
+      console.error(e);
+      this.sendMessage('ERROR', `${(e as Error).message}`);
+    }
+  };
 
   private handleMessageInternal = (message: string) => {
     console.log('Received: %s', message);
@@ -33,7 +38,7 @@ export class WsHandler {
 
     switch (parsed_message.type) {
       case 'PING':
-        this.sendMessage('PONG')
+        this.sendMessage('PONG');
         break;
       case 'SUBSCRIPTION':
         this.handleSubscription(parsed_message);
@@ -47,7 +52,7 @@ export class WsHandler {
   private handleSubscription = (parsed_message: WsInboundMessage) => {
     if (!parsed_message.body || parsed_message.body.indexOf('/') >= 0) {
       this.sendMessage('ERROR', `SUBSCRIPTION Requests need a body string that does not contain a '/' character`);
-      return
+      return;
     }
 
     const subscriptionPath: string = parsed_message.body;
@@ -63,12 +68,12 @@ export class WsHandler {
   };
 
   public handleConnect = () => {
-    writeClient(this.webSocket)
+    writeClient(this.webSocket);
     console.log(`Established connection to client with id ${this.clientId}`);
     this.sendMessage('CONNECTION_ESTABLISHED');
-  }
+  };
 
   private sendMessage = (type: WsOutboundMessageType, message?: JsonNode) => {
-    sendWsMessage(this.webSocket, type, message)
-  }
+    sendWsMessage(this.webSocket, type, message);
+  };
 }
