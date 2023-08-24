6f71d0f8ef8c997bbe3d193554a9082303732360
Adrian.Mark <adrian.mark@senacor.com>
2023-08-24 16:43:49 +0200

    Increase body size limit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createWsServer } from './server/wsServer';
 import { createHttpServer } from './server/httpServer';
 
 const app = express();
+app.use(express.json({limit: '1mb'}));
 
 // noinspection JSIgnoredPromiseFromCall
 Promise.all([
